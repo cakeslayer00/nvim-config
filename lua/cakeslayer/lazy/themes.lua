8103c11f238ef50e885e573c2cd375db90f7a491
cakeslayer00 <mainacc1838@gmail.com>
2026-06-09 21:54:32 +0500

    chore: changed color scheme, removed unnecessary bars

--- lua/cakeslayer/lazy/themes.lua
+++ lua/cakeslayer/lazy/themes.lua
@@ -1,20 +1,32 @@
 return {
-	{ 
-		"savq/melange-nvim", 
-		lazy = false,
-		priority = 1000 ,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function ()
-			require("tokyonight").setup{
-				styles = {
-					comments = { italic = false },
-					keywords = { italic = false },
-				}
-			}
-		end 
-	}
+    { 
+        "savq/melange-nvim", 
+        lazy = false,
+        priority = 1000 ,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function ()
+            require("tokyonight").setup{
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                }
+            }
+        end 
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+            })
+        end
+    }
 }
