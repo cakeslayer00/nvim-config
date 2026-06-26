e3c9d01922031cf25be5d84300c1caf69203ad7c
cakeslayer00 <mainacc1838@gmail.com>
2026-06-26 07:11:02 +0500

    add new remaps from prime

--- lua/cakeslayer/remap.lua
+++ lua/cakeslayer/remap.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
@@ -9,4 +13,4 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>yy", [["+Y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
