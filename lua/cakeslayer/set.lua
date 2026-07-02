0d165e169644d509db977cbb8f512497c509e948
cakeslayer00 <mainacc1838@gmail.com>
2026-07-02 14:34:33 +0500

    some changes

--- lua/cakeslayer/set.lua
+++ lua/cakeslayer/set.lua
@@ -2,6 +2,7 @@ vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -9,24 +10,28 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.writebackup = false
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
 vim.opt.undofile = true
 
-vim.opt.ignorecase = true 
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.mouse = "a"
 
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.showmatch = true
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-
+vim.opt.encoding = "UTF-8"
